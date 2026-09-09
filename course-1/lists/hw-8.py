7352f1a318ae1fde99953fa4cdcea3e9dd0f3462
IsoPorksu <carrotpiegoo@gmail.com>
2026-09-09 13:18:12 +0300

    Fix lists hw 8

--- course-1/lists/hw-8.py
+++ course-1/lists/hw-8.py
@@ -1,12 +1,12 @@
 words = []
 while (word := input("Please enter a word: ")) != "!":
     words.append(word)
-indices = []; words_new = words.copy()
+indices = []; words_new = []
 print("Second stage")
-while (index := int(input("Please enter an index: "))) >= 0:
-    if 0 <= index < len(words):
-        indices.append(index)
-        words_new.remove(words[index])
+while (index := int(input("Please enter an index: "))) >= 0: indices.append(index)
+for i in range(len(words)):
+    if i not in indices:
+        words_new.append(words[i])
 print("The original list of words is:", words)
 print("The list of indices is:", indices)
 print("The new list of words is:", words_new)
