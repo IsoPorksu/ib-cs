53f87c6e5c35331a1a8bce7bd7e5d1052cd53406
IsoPorksu <carrotpiegoo@gmail.com>
2026-09-11 09:08:36 +0300

    Add functions hw 7

--- course-1/functions/hw-7.py
+++ course-1/functions/hw-7.py
@@ -1,1 +1,20 @@
-hw-7.py
+"""def iterate(f, x, n):
+    result = []
+    for i in range(1, n+1):
+        result.append(f(x) if i == 1 else f(result[-1]))
+    return result
+def fun(x): return (0.5*(x+2/x))
+f = fun
+print(iterate(f, 1, 6))"""
+
+# Part 2
+def apply_functions(fs, x):
+    val = x
+    for f in reversed(fs):
+        val = f(val)
+    return val
+
+print("\nPart 2 Output:")
+fs = ['...'.join, str.split, str.lower]
+x = 'WHAT IS THIS?'
+print(apply_functions(fs, x))
